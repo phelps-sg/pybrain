e78a4da631d07fdd218d5834737f614fff373b44
Steve Phelps <sphelps@sphelps.net>
2017-05-30 10:35:01 +0100

    added SoftmaxExplorer which allows arbitrary probability fn

--- pybrain/rl/explorers/discrete/__init__.py
+++ pybrain/rl/explorers/discrete/__init__.py
@@ -1,4 +1,5 @@
 from pybrain.rl.explorers.discrete.boltzmann import BoltzmannExplorer
 from pybrain.rl.explorers.discrete.boltzmann import DiscreteExplorer
+from pybrain.rl.explorers.discrete.boltzmann import SoftmaxExplorer
 from pybrain.rl.explorers.discrete.egreedy import EpsilonGreedyExplorer
 from pybrain.rl.explorers.discrete.discretesde import DiscreteStateDependentExplorer
---
+++ pybrain/rl/explorers/discrete/softmax.py
@@ -0,0 +1,23 @@
+__author__ = "Steve Phelps, sphelps@sphelps.net"
+
+from scipy import array
+
+from pybrain.rl.explorers.discrete.discrete import DiscreteExplorer
+
+import numpy as np
+
+class SoftmaxExplorer(DiscreteExplorer):
+
+    def __init__(self, probability_fn):
+        DiscreteExplorer.__init__(self)
+        self.state = None
+        self.probability_fn = probability_fn
+
+    def activate(self, state, action):
+        self.state = state
+        return DiscreteExplorer.activate(self, state, action)
+
+    def _forwardImplementation(self, inbuf, outbuf):
+        propensities = self.module.getActionValues(self.state)
+        probabilities = self.probability_fn(propensities)
+        return np.random.choice(len(probabilities), 1, p=probabilities)
