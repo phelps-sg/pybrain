1395a2f41ca17a7c02f57dd576bea892439ef10a
Steve Phelps <sphelps@sphelps.net>
2017-05-30 10:29:08 +0100

    made DiscreteExplorer public

--- pybrain/rl/explorers/discrete/__init__.py
+++ pybrain/rl/explorers/discrete/__init__.py
@@ -1,3 +1,4 @@
 from pybrain.rl.explorers.discrete.boltzmann import BoltzmannExplorer
+from pybrain.rl.explorers.discrete.boltzmann import DiscreteExplorer
 from pybrain.rl.explorers.discrete.egreedy import EpsilonGreedyExplorer
 from pybrain.rl.explorers.discrete.discretesde import DiscreteStateDependentExplorer
